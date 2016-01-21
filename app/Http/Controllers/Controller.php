ed2e6e09a91c0debba777db8941bc1974ddfb2bf
tavo1987 <tavo198718@gmail.com>
2016-01-21 16:49:13 -0500

    Respuestas tipo json

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,14 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+
+    public function success($datos, $codigo)
+    {
+        return response()->json(['data' => $datos], $codigo);
+    }
+
+    public function error($mensaje, $codigo)
+    {
+        return response()->json(['message' => $mensaje, 'code' => $codigo], $codigo);
+    }
 }
