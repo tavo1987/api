1aeadef6b6834efe196bd0c89f7b54e8e70b0def
Edwin Ramírez <tavo198718@gmail.com>
2016-01-24 16:27:36 -0500

    Ruta para petición de access_token

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,5 +13,9 @@
 
 $app->get('/api/public/clientes', 'ClienteController@index');
 $app->get('/api/public/clientes/{id}', 'ClienteController@show');
+$app->post('/api/public/oauth/access_token', function() use($app) {
+    return response()->json($app->make('oauth2-server.authorizer')->issueAccessToken());
+});
+
 
 
